0490d6dd18a985ed3c35800bc16ab6236de8d25d
Antoinette Akinyi <61618082+Kisha-net@users.noreply.github.com>
2021-07-22 15:32:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-# GUESSING GAME
-secret_number = 9
-guess_count = 0
-guess_limit = 3
-while guess_count < guess_limit:
-    guess = int(input("Guess The Number:"))
-    guess_count += 1
-    if guess == secret_number:
-        print("You won!")
-        break
-    else:
-        print("You Lost!")
+print("Hello this is Antoinette working with python.")
